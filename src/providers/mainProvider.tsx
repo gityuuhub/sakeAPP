28fef330dd7216fa3ef2fffb6e4d0c5ccbfe1e5f
poteto <xxx@example.com>
2022-01-20 21:46:21 +0900

    プロバイダーにpropsタイプ追加

--- src/providers/mainProvider.tsx
+++ src/providers/mainProvider.tsx
@@ -1,18 +1,26 @@
 // グローバルステート管理用プロバイダー
-import { createContext, useState } from 'react';
+import React, { createContext, useState, ReactNode } from 'react';
 
 type mainContextType = {
   stubMode: boolean;
   setStubMode: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
+type PropsType = {
+  children: ReactNode
+}
+
 export const MainContext = createContext({} as mainContextType);
 
-export const MainProvider = (props: any) => {
+export const MainProvider: React.FC<PropsType> = (props: any) => {
   const { children } = props;
 
   // スタブモードのフラグ管理
   const [stubMode, setStubMode] = useState(true);
 
-  return <MainContext.Provider value={{ stubMode, setStubMode }}>{children}</MainContext.Provider>;
+  return (
+    <MainContext.Provider value={{ stubMode, setStubMode }}>
+      {children}
+    </MainContext.Provider>
+  );
 };
