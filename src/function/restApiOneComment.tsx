361a1c230eacf9b77b26fa0d66b40ff6a4a6da35
gityuuhub <93773459+gityuuhub@users.noreply.github.com>
2022-01-24 21:21:24 +0900

    Irregular whitespac対応

--- src/function/restApiOneComment.tsx
+++ src/function/restApiOneComment.tsx
@@ -1,20 +1,20 @@
-// DynamoDBにsakeOneCommnetテーブルがある。
-// 　テーブル構造
-// 　　brandId：（Number型＠パーティションキー）酒のIDが入る。
-// 　　comment：（文字列型）brandIdの酒に対するコメント。
-// DBリソースへのアクセスは、下記sakeOneCommnetのURL。
-// RestFullなAPIを目指す。
-// 　メソッド
-// 　Get：バラメータにbrandIdを指定することで登録されているコメントを返す。
-// 　　こんな感じ：https://url?brandId=0
-// 　　※レスポンスはAPI-GWの総合レスポンスで整形。
-//      JSONのNumber型でも""括りが残っているが妥協。
-// 　Post：バラメータでbrandIdとcommentを指定した内容をDBに登録できる。
-// 　　※パーテションキーが重複したら上書きされるっぽい。RDSと勝手が違う。
-// 　　こんな感じ：https://url
-// 　　　　　　　　body{"brandId":0,"comment":"レモンサワーからの梅酒（売り切れ直前）"}
-// ノーガードなので変なアクセス受けると店じまい予定。
-
+/*
+DynamoDBにsakeOneCommnetテーブルがある。
+  テーブル構造
+    brandId：（Number型＠パーティションキー）酒のIDが入る。
+    comment：（文字列型）brandIdの酒に対するコメント。
+  DBリソースへのアクセスは、下記sakeOneCommnetのURL。
+  RestFullなAPIを目指す。
+　メソッド
+　  Get：バラメータにbrandIdを指定することで登録されているコメントを返す。
+      こんな感じ：https://url?brandId=0
+      ※レスポンスはAPI-GWの総合レスポンスで整形。
+    Post：バラメータでbrandIdとcommentを指定した内容をDBに登録できる。
+      ※パーテションキーが重複したら上書きされるっぽい。RDSと勝手が違う。
+      こんな感じ：https://url
+      body{"brandId":0,"comment":"レモンサワーからの梅酒（売り切れ直前）"}
+ノーガードなので変なアクセス受けると店じまい予定。
+*/
 import axios from 'axios';
 
 const sakeOneCommnetUrl =
